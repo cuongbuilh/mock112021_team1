e950e0965775132dd259a97df926975760d13fc9
cuongbui <cuongbuilh@gmail.com>
2021-12-10 14:12:50 +0700

    add jpa depen...

--- src/main/java/com/vti/mock/model/Program.java
+++ src/main/java/com/vti/mock/model/Program.java
@@ -1,4 +1,21 @@
 package com.vti.mock.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "program", catalog = "mock_t11")
 public class Program {
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
 }
